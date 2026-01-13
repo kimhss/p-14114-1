ed401f7b10254791994b997aaa7418561179e754
kimhss <hyunuary108@kyonggi.ac.kr>
2026-01-13 19:29:44 +0900

    118강 work

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -12,6 +12,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -120,12 +123,15 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             rq.setHeader("Authorization", actorAccessToken);
         }
 
-        UserDetails user = new SecurityUser(
+        Collection<? extends GrantedAuthority> authorities = member.isAdmin() ?
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN")) : List.of();
+
+                UserDetails user = new SecurityUser(
                 member.getId(),
                 member.getUsername(),
                 "",
                 member.getName(),
-                List.of()
+                authorities
         );
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(
