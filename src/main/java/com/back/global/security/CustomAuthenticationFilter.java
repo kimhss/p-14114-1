657cb0992d39ee6747aa9e0a106b9bcd4bc97c38
kimhss <hyunuary108@kyonggi.ac.kr>
2026-01-13 20:51:30 +0900

    125강 work

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -30,6 +31,7 @@ import java.util.Map;
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
     private final MemberService memberService;
     private final Rq rq;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -41,12 +43,9 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             RsData<Void> rsData = e.getRsData();
             response.setContentType("application/json");
             response.setStatus(rsData.statusCode());
-            response.getWriter().write("""
-                    {
-                        "resultCode": "%s",
-                        "msg": "%s"
-                    }
-                    """.formatted(rsData.resultCode(), rsData.msg()));
+            response.getWriter().write(
+                    objectMapper.writeValueAsString(rsData)
+            );
         } catch (Exception e) {
             throw e;
         }
